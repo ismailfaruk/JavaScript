d019761f14fac08bbf5a5e43a952d098db2addd0
ismailfaruk <ismailfaruk.17@gmail.com>
2019-03-21 08:11:01 -0400

    minor housekeeping

---
+++ MyFirstProject/sec5lec18.js
@@ -0,0 +1,9 @@
+/*Programming Challenge: Gym Membership Fees Increase
+Section 5, Lecture 18
+
+A gym currently charges $29.99 per month.  It has stated that it raises its monthly fee at the start of every year by 2%.
+
+Write a program which uses a for loop to display the expected rates year by year for a number of years from now between 3 and 10 years 
+(this means you should generate a random integer between 3 and 10).  For example, if the randomly generated year was 4, then the program 
+would log 4 statements to the console which each say the year followed by the expected monthly fee for that year.*/
+
